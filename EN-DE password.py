fb3900c276737ef00810b4ea5103c80d6de9db45
Mrmahdi <ahangari138010@gmail.com>
2022-06-11 11:45:22 -0700

    structure without functionality/graphic

--- EN-DE password.py
+++ EN-DE password.py
@@ -1,26 +1,46 @@
 from tkinter import *
+from turtle import width
 
 Window = Tk()
-Window.geometry("400x450+500+200")
+Window.geometry("400x400+500+200")
 Window.title("password manager")
 Window.iconbitmap(bitmap="gray75")
 
-f1 = Frame(Window)
-f1.pack(pady=20)
-
+f1 = Frame(Window)        # frame 1 button ha dakheleshe
+f1.pack(pady=20,)
+# -----------------// command button ha //-----------------
 def encrypt():
     pass
 def decrypt():
     pass
 def clear():
     pass
+# -----------------// command button ha //-----------------
 
-en_button = Button(f1, text=" ",padx=50, command=encrypt)
-de_button = Button(f1, text=" ",padx=50, command=decrypt)
-clear_button = Button(f1, text=" ",padx=50, command=clear)
+# -----------------// button ha //-----------------
+en_button = Button(f1, text="encrypt", padx=20, font=("Helvetica", 14), command=encrypt)
+de_button = Button(f1, text="decrypt", padx=20, font=("Helvetica", 14), command=decrypt)
+clear_button = Button(f1, text="clear", padx=20, font=("Helvetica", 14), command=clear)
+# -----------------// button ha //-----------------
 
-en_button.grid(row=0, column=1)
-de_button.grid(row=0, column=2)
-clear_button.grid(row=0, column=3)
+# -----------------// Position button ha //-----------------
+en_button.grid(row=0, column=1, padx=10)
+de_button.grid(row=0, column=2, padx=10)
+clear_button.grid(row=0, column=3, padx=10)
+# -----------------// Position button ha //-----------------
+
+# -----------------// body //-----------------
+Tozihat = Label(Window, text="Encrypted/Decrypted text !!! ", font=("Helvetica", 14))
+Tozihat.pack(pady=10)
+
+output = Text(Window, width=45, height=10)
+output.pack(pady=10)
+
+Tozihat2 = Label(Window, text="Enter your password", font=("Helvetica", 14))
+Tozihat2.pack()
+
+entery = Entry(Window, width=60, show= "*")
+entery.pack()
+# -----------------// body //-----------------
 
 Window.mainloop()
