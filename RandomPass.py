17d2028ac4031d47dd7f46d678ed5d6c82274624
Mrmahdi <ahangari138010@gmail.com>
2022-06-12 09:35:38 -0700

    copy system added

--- RandomPass.py
+++ RandomPass.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import random
 from tkinter.font import ITALIC
+from tkinter import messagebox
+import clipboard
 
 def Runapp():
     window = Tk()
@@ -12,7 +14,7 @@ def Runapp():
 
     # RamzNow = ""
 
-
+# -----------------// command button ha //-----------------
     def createrndm():
         #65 - 122
         Ramz = ""
@@ -26,7 +28,15 @@ def Runapp():
 
     def ClearAll():
         MONITOR.delete(1.0, END)      
+    
 
+    def copykon():
+        clipboard.copy(MONITOR.get(1.0, "end-1c"))
+        messagebox.showinfo("Copy", "Password Copied to clip board seccesfully") 
+        ClearAll()
+
+
+# -----------------// command button ha //-----------------
 
     MONITOR = Text(window, bg='silver', width=10, height=0, bd=3, font=("Helvetica", 20), padx=25, pady=30)
     MONITOR.grid(row=0, column=0, columnspan=3, pady=10, padx=25)
@@ -34,16 +44,23 @@ def Runapp():
 
     Guide_T = Label(
         window, text="click button to Create a random password")
-    Guide_T.grid(row=1, column=0, pady=10, padx=5, columnspan=3)
+    Guide_T.grid(row=1, column=0, pady=5, padx=5, columnspan=3)
 
+# -----------------// button ha //-----------------
     Create = Button(window, text="create", width=13,
-                    height=2, bd=3, background="lightgray", command=createrndm)
-    Create.grid(row=2, column=0, pady=10, padx=5)
+                    height=2, bd=4, background="lightgray", command=createrndm)
+    Create.grid(row=2, column=0, pady=5, padx=5)
     
     Clear = Button(window, text="clear", width=13,
-                    height=2, bd=3, background="lightgray", command=ClearAll)
+                    height=2, bd=4, background="lightgray", command=ClearAll)
     Clear.grid(row=2, column=1)
 
+    Copyto = Button(window, text="Copy to Clipboard", width=14,  #  gets what is inside and copy it to clipboard
+                    height=2, bd=3, background="lightgray", command= copykon)
+    Copyto.grid(row=3, column=0)
+
+    Exit = Button(window, text="EXIT", padx=10, bg='gray', relief="groove", bd= 5, font=("Helvetica", 10), command=window.destroy)
+    Exit.grid(row=3, column=1, pady=5, padx=5)
+# -----------------// button ha //-----------------
 
     window.mainloop()
-# Runapp()
